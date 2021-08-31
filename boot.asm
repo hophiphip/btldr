578c5be9c2a34e6ed9936fa56c9b691e7c88bfe0
hophiphip <nighop@yandex.ru>
2021-08-31 23:42:10 +0400

    Note on fixing incorrect cell error

--- boot.asm
+++ boot.asm
@@ -68,7 +68,7 @@ entry:
 
   ; TESTING BEGIN ===============================================
   ; NOTE: `get_cell_pixel` is not correct for indexes > 0 
-  mov ax, 0
+  mov ax, 1
   call get_cell_pixel
   mov di, ax
   xor ax, ax
@@ -139,13 +139,15 @@ get_cell_pixel:
     mov cx, FIELD_WIDTH ; cx = FIELD_WIDTH
     div cx              ; (dx:ax) == (000...ax) / cx ==> ax <- div & dx <- mod
 
-    mov bx, CELL_HEIGHT ; bx = CELL_HEIGHT
-    mul bx              ; ax *= bx
-    add ax, FIELD_START ; ax += FIELD_START 
-
+    mov bx, CELL_HEIGHT  ; bx = CELL_HEIGHT
+    mul bx               ; ax *= bx
+    mov bx, SCREEN_WIDTH ; bx = SCREEN_WIDTH
+    mul bx               ; ax *= bx
+    add ax, FIELD_START  ; ax += FIELD_START 
+    ; NOTE: Sth wrong with X 
     mov bx, ax          ; bx = ax
     mov ax, CELL_WIDTH  ; ax = CELL_WIDTH
-    mul dx              ; ax *= dx
+    mul dl              ; ax *= dx
     
     add bx, ax          ; bx += ax
     mov ax, bx          ; ax = bx
